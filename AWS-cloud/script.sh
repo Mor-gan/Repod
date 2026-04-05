ee92be45f8d4795a373ad997763727ea52abc536
Mor-gan <ogundipeabimbola9@gmail.com>
2026-04-05 15:47:59 +0100

    automating ec2 page with an s3 bucket

--- AWS-cloud/script.sh
+++ AWS-cloud/script.sh
@@ -6,4 +6,7 @@ sudo systemctl enable nginx
 sudo nano /usr/share/nginx/html/index.html
 
 ## EC2 -> S3 
-
+echo "Hello World from Nginx" > index.html
+sudo nano /usr/share/nginx/html/index.html
+-<iframe src="https://firstservers3.s3.amazonaws.com/index.html "width="100%" height="500"></iframe>
+sudo aws s3 cp s3://firstservers3/index.html /usr/share/nginx/html/index.html
